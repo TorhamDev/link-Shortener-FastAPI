90d5da417e85c12057760ff218a95ad046f7b544
Torham <torhamdev@gmail.com>
2023-01-20 18:24:41 +0330

    Create Link database mode

---
+++ models.py
@@ -0,0 +1,18 @@
+import peewee
+
+from database import db
+
+
+class Link(peewee.Model):
+    address = peewee.CharField(index=True)
+    short_link = peewee.CharField(index=True, max_length=8)
+
+    class Meta:
+        database = db
+
+
+
+db.connect()
+db.create_tables([Link], safe = True)
+db.close()
+
