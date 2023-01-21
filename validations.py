f862f06d142ab7e29f375269cc5249c4da93f9fc
Torham <torhamdev@gmail.com>
2023-01-21 23:11:12 +0330

    Add description for link_validation function

--- validations.py
+++ validations.py
@@ -3,6 +3,13 @@ from fastapi import HTTPException
 
 
 def link_validation(link: str) -> bool:
+    """
+    Validate user input link to see if a real url has been entered or not
+
+    params : link : user input link
+
+    return : True, False or HTTPException
+    """
 
     url_http_https = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
     url_no_http_https = "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
