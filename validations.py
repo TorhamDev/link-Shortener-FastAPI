19c5cc6c7bb085a465c015c5a2eee180854a88da
Torham <torhamdev@gmail.com>
2023-01-21 22:55:57 +0330

    Handle Http Error in validation function

--- validations.py
+++ validations.py
@@ -1,14 +1,15 @@
-import re
+from re import match
+from fastapi import HTTPException
 
 
-def link_validation(link: str):
-    
+def link_validation(link: str) -> bool:
+
     url_http_https = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
     url_no_http_https = "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
 
-    if re.match(url_http_https, link):
+    if match(url_http_https, link):
         return True
-    elif re.match(url_no_http_https, link):
+    elif match(url_no_http_https, link):
         return True
     else:
-        return False
+        raise HTTPException(status_code=400, detail="Invalid link address")
