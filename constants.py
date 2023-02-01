121258b9a70a802a230b785e1224e8a01180f92b
Torham <torhamdev@gmail.com>
2023-02-01 18:20:37 +0330

    Get constants from shell env variables

--- constants.py
+++ constants.py
@@ -1,17 +1,18 @@
+from os import getenv
+
 # MySQL configs
-DB_NAME = "shortener_link"
-DB_USER = "root"
-DB_PASSWORD = "mysql"
-DB_HOST = "localhost"
-DB_PORT = 3306
+DB_NAME = getenv("DB_NAME")
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
 
 # Redis configs
-REDIS_DB = 0
-REDIS_DB_HOST = "localhost"
-REDIS_DB_PORT = 6379
+REDIS_DB = getenv("REDIS_DB")
+REDIS_DB_HOST = getenv("REDIS_DB_HOST")
+REDIS_DB_PORT = getenv("REDIS_DB_PORT")
 
 # Regex for link validation
 WITH_HTTP = "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
 WITH_HTTPS = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]" +\
              "{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-
