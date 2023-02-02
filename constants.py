d510d4840495a5a1d14b02d03e5ea27c6f4d3d8e
Torham <torhamdev@gmail.com>
2023-02-02 10:12:20 +0330

    Revert "Conver db port from str to int"
    This reverts commit 3251ba5a0fb79951b4f29e24ce5e4d82666d4040.

--- constants.py
+++ constants.py
@@ -5,12 +5,12 @@ DB_NAME = getenv("DB_NAME")
 DB_USER = getenv("DB_USER")
 DB_PASSWORD = getenv("DB_PASSWORD")
 DB_HOST = getenv("DB_HOST")
-DB_PORT = int(getenv("DB_PORT"))
+DB_PORT = getenv("DB_PORT")
 
 # Redis configs
 REDIS_DB = getenv("REDIS_DB")
 REDIS_DB_HOST = getenv("REDIS_DB_HOST")
-REDIS_DB_PORT = int(getenv("REDIS_DB_PORT"))
+REDIS_DB_PORT = getenv("REDIS_DB_PORT")
 
 # Regex for link validation
 WITH_HTTP = "^[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
